577befbb0ae413321f0a7c0c1c863e7532848a67
Gersoncs107 <gerson.silva107@gmail.com>
2025-05-25 22:00:50 -0300

    part1-react

--- src/App.js
+++ src/App.js
@@ -1,19 +1,5 @@
 import { useState } from "react"
 
-const Hello = ({nome, idade})=>{
-
-  const anoDenascimento = () => {
-    const anoAtual = new Date().getFullYear()
-    return anoAtual - idade  
-  } 
-  return(
-    <div>
-      <p>Olá {nome} você tem {idade}</p>
-      <p>Então você nasceu provavelmente em {anoDenascimento()}</p>
-    </div>
-  )
-}
-
 const Exibir = ({contador}) => {
   return <div><h1>{contador}</h1></div>
 }
